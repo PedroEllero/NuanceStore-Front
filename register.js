1b9a0e7e570c489f6ffbe81252f026c892d05504
ruanazevedo <ruannzeroh@gmail.com>
2024-04-18 16:01:02 -0300

    adicionando estrutura basica do front-end

---
+++ register.js
@@ -0,0 +1,6 @@
+const buttonRegister = document.getElementById("buttonRegister")
+const buttonLogin = document.getElementById("buttonLogin")
+const a = document.getElementsByClassName("input").txt.value
+
+buttonLogin.addEventListener("click", () => alert("login feito com sucesso!"))
+buttonRegister.addEventListener("click", () => alert(a))
